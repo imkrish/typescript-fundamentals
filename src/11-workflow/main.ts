4e401edc4502d68c3f4cdf913c9bcfb2eae4fb66
Keerati Limkulphong <imKrish@Keeratis-MacBook-Pro.local>
2016-10-13 08:58:41 +1300

    Manage excludes:[] in tsconfig.json

---
+++ src/11-workflow/main.ts
@@ -0,0 +1,26 @@
+namespace typescript_workflow {
+
+    // --------------------------- Normal Workflow ----------------------------------
+
+    // 1. normal workflow
+
+    // <script src="node_modules/systemjs/dist/system.js"></script>
+    // SystemJS.config({
+    //     map: {
+    //         'jQuery': '/node_modules/jquery/dist/jquery.min.js'
+    //     },
+    //     baseURL: '/src/10-jquery',
+    //     defaultJSExtensions: true
+    // })
+
+    // SystemJS.import('main.js')
+
+    // 2. setup tsconfig.json
+    // 3. tsc -w
+
+    // tsc app.ts    (compile only one file)
+    // tsc app.ts --sourceMap
+    // tsc -p config/tsconfig.json
+
+    // --------------------------- Normal Workflow ----------------------------------
+}
