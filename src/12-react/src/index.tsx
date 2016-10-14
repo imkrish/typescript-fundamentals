c21f4db781dc1aa015c2cf92648b98a49d38f38c
Keerati Limkulphong <imKrish@Keeratis-MacBook-Pro.local>
2016-10-15 09:48:46 +1300

    React with TypeScript Finish the course

---
+++ src/12-react/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import { Home } from './components/Home'
+
+ReactDOM.render(<Home name="Krish", age={25}/>, document.getElementById('app'))
